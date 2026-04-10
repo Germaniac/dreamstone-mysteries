80885d748b58ac16babc5d39b08fd5b904f2e82e
Germaniac <germaniachaxball@gmail.com>
2026-04-10 22:33:14 +0700

    Update form_change_tables.h

--- src/data/pokemon/form_change_tables.h
+++ src/data/pokemon/form_change_tables.h
@@ -246,6 +246,16 @@ static const struct FormChange sHeracrossFormChangeTable[] = {
 };
 #endif //P_FAMILY_HERACROSS
 
+#if P_FAMILY_SKARMORY
+static const struct FormChange sSkarmoryFormChangeTable[] =
+{
+    {FORM_CHANGE_BATTLE_MEGA_EVOLUTION_ITEM,    SPECIES_SKARMORY_MEGA, ITEM_SKARMORITE},
+    {FORM_CHANGE_FAINT,                         SPECIES_SKARMORY},
+    {FORM_CHANGE_END_BATTLE,                    SPECIES_SKARMORY},
+    {FORM_CHANGE_TERMINATOR},
+};
+#endif //P_FAMILY_SKARMORY
+
 #if P_FAMILY_HOUNDOUR
 static const struct FormChange sHoundoomFormChangeTable[] = {
 #if P_MEGA_EVOLUTIONS
